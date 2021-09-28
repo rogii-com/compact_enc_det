09717d028efe70d2236e49e18fbe3a577cfacd4a
Alexander Shkarlatov <a.shkarlatov@rogii.com>
2021-09-28 16:55:10 +0400

    Updated build scripts

--- build_x86.cmake
+++
@@ -1,4 +0,0 @@
-EXECUTE_PROCESS(
-    COMMAND
-    ${CMAKE_COMMAND} -E time ${CMAKE_COMMAND} -P "${CMAKE_CURRENT_LIST_DIR}/cmake/package/build_x86.cmake"
-)
--- cmake/package/build_amd64.cmake
+++
@@ -1,8 +0,0 @@
-SET(
-    ARCH
-    amd64
-)
-
-INCLUDE(
-    "${CMAKE_CURRENT_LIST_DIR}/build_common.cmake"
-)
